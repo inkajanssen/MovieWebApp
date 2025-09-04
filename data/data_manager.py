1916a51e9d05b8250bffb9365cbd4de758612bac
inkajanssen <ijanssenuk@gmail.com>
2025-09-04 18:37:58 +0200

    created datamanager with CRUD methods

---
+++ data/data_manager.py
@@ -0,0 +1,74 @@
+from models import db, User, Movies, UsersFavoriteMovie
+
+class DataManager():
+    """
+    Class to interact with a SQLite database.
+    Implements all the basic CRUD methods.
+    """
+
+    def create_user(self, name):
+        """
+        Add a new user to your database.
+        """
+        new_user = User(name=name)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return "User sucessfully added to the database."
+
+
+    def get_users(self):
+        """
+        Return a list of all users in your database.
+        """
+        users = User.query.order_by(User.name).all()
+        if users:
+            return users
+        return []
+
+
+    def get_movies(self, user_id):
+        """
+        Return a list of all movies of a specific user.
+        """
+        user = User.query.get(user_id)
+        if user:
+            return user.favorite_movies.all()
+        return []
+
+
+    def add_movie(self, user_id, movie):
+        """
+        Add a new movie to a user’s favorites.
+        """
+        user = User.query.get(user_id)
+        new_movie = Movies.query.filter_by(name=movie['name']).first()
+
+        user.favorite_movies.append(new_movie)
+        db.session.commit()
+
+        return "Movie sucessfully added to the favorites."
+
+
+    def update_movie(self, movie_id, new_title):
+        """
+        Update the details of a specific movie in the database.
+        """
+        movie_to_update = Movies.query.filter_by(id = movie_id).first()
+        movie_to_update.name = new_title
+        db.session.commit()
+
+        return "Movie successfully updated"
+
+
+    def delete_movie(self, user_id, movie_id):
+        """
+        Delete the movie from the user’s list of favorites.
+        """
+        user = User.query.get(user_id)
+        movie_to_remove = Movies.query.get(movie_id)
+
+        user.favorite_movies.remove(movie_to_remove)
+        db.session.commit()
+
+        return "Movie sucessfully removed from favorites"
